2735b52055be12db972647ef1fe2b5f21c049275
rksaxena0101 <rajatsaxena169@gmail.com>
2024-06-16 17:16:50 +0530

    "Initial changes"

---
+++ taskmanager/src/main/java/com/rajat/taskmanager_spring_java/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.taskmanager_spring_java.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
